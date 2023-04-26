299522fa2727129923524d3a01100d612adff9fe
evawanini <evelynwanini14@gmail.com>
2023-04-26 21:23:53 +0300

    Set up google auth and auth screens

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,38 +2,18 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 use App\Models\Scam;
-=======
->>>>>>> 45b0c05 (Set up google auth and auth screens)
-=======
-use App\Models\Scam;
->>>>>>> 4494bb2 (Add users and scams sections for admin)
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-use function PHPSTORM_META\type;
-
-=======
->>>>>>> 45b0c05 (Set up google auth and auth screens)
-=======
-use function PHPSTORM_META\type;
-
->>>>>>> 4494bb2 (Add users and scams sections for admin)
 class HomeController extends Controller
 {
     public function index ()
     {
         $role = Auth::user()->getRoleNames()->first();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4494bb2 (Add users and scams sections for admin)
+
         $users_count = User::all()->count();
         $scams_count = Scam::all()->count();
 
@@ -43,7 +23,6 @@ class HomeController extends Controller
                 'scams_count' => $scams_count,
             ]);
         } else {
-<<<<<<< HEAD
             $user = Auth::user();
             $scams = Scam::withTrashed()->with('user')->paginate(3);
             
@@ -52,16 +31,5 @@ class HomeController extends Controller
                 'scams' => $scams
             ]);
         }
-
-=======
-        return Inertia::render('Dashboard', [
-            'role'=>$role
-        ]);
->>>>>>> 45b0c05 (Set up google auth and auth screens)
-=======
-            return Inertia::render('UserDash');
-        }
-
->>>>>>> 4494bb2 (Add users and scams sections for admin)
     }
 }
